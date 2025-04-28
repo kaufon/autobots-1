80a83625d23c5861e106602352f376e293a0165b
kaufon <kauanfvale@gmail.com>
2025-04-28 19:08:21 -0300

    add client crud

---
+++ src/main/java/com/autobots/automanager/controles/ClienteControle.java
@@ -0,0 +1,77 @@
+package com.autobots.automanager.controles;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.autobots.automanager.entidades.Cliente;
+import com.autobots.automanager.modelo.ClienteAtualizador;
+import com.autobots.automanager.modelo.ClienteSelecionador;
+import com.autobots.automanager.modelo.DocumentoAtualizador;
+import com.autobots.automanager.repositorios.ClienteRepositorio;
+import com.autobots.automanager.repositorios.DocumentoRepositorio;
+import com.autobots.automanager.repositorios.EnderecoRepositorio;
+import com.autobots.automanager.repositorios.TelefoneRepositorio;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteControle {
+	@Autowired
+	private ClienteRepositorio repositorio;
+
+  @Autowired
+  private DocumentoRepositorio documentoRepositorio;
+
+  @Autowired
+  private TelefoneRepositorio telefoneRepositorio;
+
+	@Autowired
+	private ClienteSelecionador selecionador;
+
+	@GetMapping("/cliente/{id}")
+	public Cliente obterCliente(@PathVariable long id) {
+		List<Cliente> clientes = repositorio.findAll();
+		return selecionador.selecionar(clientes, id);
+	}
+
+	@GetMapping("/clientes")
+	public List<Cliente> obterClientes() {
+		List<Cliente> clientes = repositorio.findAll();
+		return clientes;
+	}
+
+	@PostMapping("/cadastro")
+	public void cadastrarCliente(@RequestBody Cliente cliente) {
+		repositorio.save(cliente);
+    cliente.getDocumentos().forEach(documento -> {
+      documento.setCliente(cliente);
+      documentoRepositorio.save(documento);
+    });
+    cliente.getTelefones().forEach(telefone -> {
+      telefone.setCliente(cliente);
+      telefoneRepositorio.save(telefone);
+    });
+	}
+
+	@PutMapping("/atualizar")
+	public void atualizarCliente(@RequestBody Cliente atualizacao) {
+		Cliente cliente = repositorio.getById(atualizacao.getId());
+		ClienteAtualizador atualizador = new ClienteAtualizador();
+		atualizador.atualizar(cliente, atualizacao);
+		repositorio.save(cliente);
+	}
+
+	@DeleteMapping("/excluir")
+	public void excluirCliente(@RequestBody Cliente exclusao) {
+		Cliente cliente = repositorio.getById(exclusao.getId());
+		repositorio.delete(cliente);
+	}
+}
